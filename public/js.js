fb54be084b4709d5761835d8893b0f691d7819d5
lucasmdf07 <38864269+lucasmdf07@users.noreply.github.com>
2019-12-02 21:53:15 -0700

    videos

--- public/js.js
+++ public/js.js
@@ -5,35 +5,22 @@ const url = 'https://api.themoviedb.org/3/search/movie?api_key=243617934df15cd41
 const searchButton = document.querySelector('#searchSubmit');
 const searchInput = document.querySelector('#inputSearchBox');
 const movieSearchable = document.querySelector('#movies-searchable');
-const IMAGE_URL = 'https://image.tmdb.org/t/p/w500'
+const IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
+const imgElement = document.querySelector('img');
 
-{
-    /* <div class="movie">
-        <section class="section">
-            <img
-                src=""
-                alt=""
-                data-movie-id="557"
-            />
-            <img
-                src=""
-                alt=""
-                data-movie-id="557"
-            />
-        </section>
-        <div class="content">
-            <p id="content-close">X</p>
-        </div>
-    </div> */
+function generateUrl(path) {
+    const url = `https://api.themoviedb.org/3${path}?api_key=243617934df15cd416cb4fab0608b4ff`;
+    return url;
 }
 
 function movieSelection(movies) {
     return movies.map((movie) => {
         // if statement to make sure we do not display paths without images
         if (movie.poster_path) {
-            return `
-                <img src=${IMAGE_URL + movie.poster_path} data-movie-id=${movie.id}/> 
-                `;
+            return `<img 
+                src=${IMAGE_URL + movie.poster_path} 
+                data-movie-id=${movie.id}
+            />`;
         }
     })
 }
@@ -47,7 +34,7 @@ function createMovieContainer(movies) {
             ${movieSelection(movies)}
         </section>
         <div class = "content">
-        <p id = "content-close"></p>
+        <p id = "content-close">X</p>
         </div>  
     `;
 
@@ -55,29 +42,95 @@ function createMovieContainer(movies) {
     return movieElement;
 }
 
+function renderSearchMovies(data) {
+    // data.results []
+    movieSearchable.innerHTML = '';
+    const movies = data.results;
+    const movieBlock = createMovieContainer(movies);
+    movieSearchable.appendChild(movieBlock);
+    console.log('Data: ', data);
+
+}
+
 searchButton.onclick = function (event) {
     event.preventDefault();
     const value = searchInput.value;
-    const newUrl = url + '&query=' + value;
+    const path = '/search/movie';
+    const newUrl = generateUrl(path) + '&query=' + value;
 
     fetch(newUrl)
         .then((res) => res.json())
-        .then((data) => {
-
-            // data.results []
-            movieSearchable.innerHTML = '';
-            const movies = data.results;
-            const movieBlock = createMovieContainer(movies);
-            movieSearchable.appendChild(movieBlock);
-            console.log('Data: ', data);
-        })
+        .then(renderSearchMovies)
         .catch((error) => {
             console.log('Error: ', error);
         });
-    
-    
+
+
     searchInput.value = '';
 
     console.log('value: ', value);
 
+}
+
+// function createReviews() {
+
+// }
+
+function createIframe(video) {
+    const iframe = document.createElement('iframe');
+    iframe.src = `https://www.youtube.com/embed/${video.key}`;
+    iframe.width = 360;
+    iframe.height = 315;
+    iframe.allowFullscreen = true;
+
+    return iframe;
+}
+
+function createVideoTemplate(data, content) {
+    content.innerHTML = '<p id="content-close">X</p>';
+    console.log('Videos: ', data);
+    const videos = data.results;
+    const length = videos.length > 4 ? 4 : videos.length;
+    const iframeContainer = document.createElement('div');
+
+    for (let i = 0; i < length; i++) {
+        const video = videos[i];
+        const iframe = createIframe(video);
+        iframeContainer.appendChild(iframe);
+        content.appendChild(iframeContainer);
+    }
+}
+
+
+// Event Delegation
+document.onclick = function (event) {
+
+    const target = event.target;
+
+    if (target.tagName.toLowerCase() === 'img') {
+        // console.log('Hello World');
+        const movieId = target.dataset.movieId;
+        console.log('MovieId: ', movieId);
+        const section = event.target.parentElement;
+        const content = section.nextElementSibling;
+        content.classList.add('content-display');
+
+        const path = `/movie/${movieId}/videos`;
+        const url = generateUrl(path);
+
+        //fetch movie videos
+        fetch(url)
+            .then((res) => res.json())
+            .then((data) => createVideoTemplate(data, content))
+            .catch((error) => {
+                console.log('Error: ', error);
+            });
+
+    }
+
+    if (target.id === 'content-close') {
+        const content = target.parentElement;
+        content.classList.remove('content-display');
+
+    }
 }
