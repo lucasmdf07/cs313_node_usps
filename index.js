5186cda30d198545455c764e182c55dc0873045e
lucasmdf07 <38864269+lucasmdf07@users.noreply.github.com>
2019-12-09 01:42:04 -0700

    login added

--- index.js
+++ index.js
@@ -1,16 +1,120 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-var engine = require("./math.js")
+const express = require('express');
+const path = require('path');
+const axios = require('axios')
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000;
+const app = express();
 
-const app = express()
+const session = require('express-session')
 
-app.use(express.static(path.join(__dirname, 'public'))).set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-app.get('/', (req, res) => {
-        res.sendFile("form.html", {
-            root: __dirname + "/public"
-        })
-})
-app.get('/result', engine.getParams)
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(session({
+  secret: 'vimeo-secret',
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false});
+
+app.use(express.json() );
+app.use(express.urlencoded({ extended: false }));
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
+app.get('/', verifyLogin, (req, res) => {
+    getGenres()
+    .then(response => {
+      res.render('project2/index', {data: response['data']['data']})
+    })
+    .catch(error => {
+      res.send("fail" + error)
+    })
+  })
+
+app.get('/genres/:id', verifyLogin, (req, res) => {
+    getGenre(req.params.id)
+    .then(response => {
+      const data = [];
+      response['data']['data'].forEach(item => {data.push(item)});
+
+      data.forEach(item => {
+          // Creating the poster and background image of dimensions 250 by 375
+          let pictures = item.pictures.uri;
+          item.pictures = "https://i.vimeocdn.com/vod_poster" + pictures.substring(pictures.lastIndexOf("/"));
+
+          // Reducing the description to 40 words
+          item.description = item.description.split(" ").splice(0,40).join(" ") + "...";
+      });
+      res.render('project2/genres', {data: data, selected: req.params.id})
+    })
+    .catch(error => {
+      res.send("fail" + error)
+    })})
+
+app.get('/login', (req, res) => {
+  res.render('project2/login')
+});
+
+app.post('/login', handleLogin);
+app.get('/logout', handleLogout)
+
+function getGenres() {
+  try {
+    return axios.get('https://api.vimeo.com/ondemand/genres?access_token=675158fe38a61d2601b865875b53d506&fields=name,canonical')
+  } catch (error) {
+    console.error(error)
+  }
+}  
+
+function getGenre(genre) {
+  try {
+    return axios.get('https://api.vimeo.com/ondemand/genres/' + genre + '/pages/?access_token=675158fe38a61d2601b865875b53d506&fields=name,description,link,colors,pictures.uri')
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+function handleLogin(req, res) {
+  var result = {success: false};
+
+  if (req.body.username == "lucas" && req.body.password == "1234") {
+    req.session.user = req.body.username;
+    result = {success: true};
+  }
+
+  res.redirect('back');
+}
+
+function handleLogout(req, res) {
+  if (req.session.user) {
+    req.session.destroy();
+  }
+
+  res.redirect('/')
+}
+
+function verifyLogin(req, res, next) {
+  if (req.session.user) {
+    // logged in
+    next();
+  } else {
+    // not logged in
+    res.render('project2/login')
+  }
+}
+
+function logRequest(req, res, next) {
+  console.log("Received a request for: " + req.url);
+
+  next();
+}
--- public/validate.js
+++
@@ -1,8 +0,0 @@
-function validate() {
-    var doc = document.getElementById("wt")
-    var reg2 = /\d+[.]?/
-    if (reg2.test(doc.value) != true) {
-        document.getElementById("bad").innerHTML = "Please type numbers."
-        return false
-    }
-}   
